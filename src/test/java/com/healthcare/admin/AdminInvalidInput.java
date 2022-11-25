ea2eff2603797c44e7348f2013ae014ba95bf9ec
Khushal Abrol <khushalabrol14@gmail.com>
2022-11-25 19:20:46 +0530

    Updated: pom.xml, Added: AdminInvalidInput

---
+++ src/test/java/com/healthcare/admin/AdminInvalidInput.java
@@ -0,0 +1,15 @@
+package com.healthcare.admin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.IOException;
+
+public class AdminInvalidInput {
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+    }
+}
